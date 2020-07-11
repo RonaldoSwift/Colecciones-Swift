695191023b3cd2fb9dc31a842f29d3d6fa223093
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-07-11 10:48:28 -0500

    Initial Commit

---
+++ Colecciones1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colecciones1
+//
+//  Created by Ronaldo Andre Vargas Huaman on 7/11/20.
+//  Copyright © 2020 Ronaldo Andre Vargas Huaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

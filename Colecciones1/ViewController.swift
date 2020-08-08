2671081f324caacc3408a106812e266258e21443
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2020-08-08 13:38:16 -0500

    resuelto ejercicio de COMPOSICION

--- Colecciones1/ViewController.swift
+++ Colecciones1/ViewController.swift
@@ -16,14 +16,34 @@ class ViewController: UIViewController {
     /* Crear una estructura llamada Hamburguesa que este compuesta por las estructuras Lechuga, Tomate, Papa, Carne, Pan. Finalmente crear un arreglo de Hamburguesa cuando se haga click al boton.
      */
     
-    // PUT YOUR CODE HERE
-
+    struct Hamburguesa {
+        var lechuga: Lechuga
+        var tomate: Tomate
+        var papa: Papa
+        var carne: Carne
+        var pan: Pan
+    }
+    struct Lechuga {
+    }
+    struct Tomate{
+    }
+    struct Papa{
+    }
+    struct Carne{
+    }
+    struct Pan{
+    }
+    
+    
+    
     override func viewDidLoad() {
            super.viewDidLoad()
     }
     
     @IBAction func realizarAlgo(_ sender: Any) {
-        
+        var hamburguesas: Array<Hamburguesa> = Array()
+        hamburguesas.append(Hamburguesa(lechuga: Lechuga(), tomate: Tomate(), papa: Papa(), carne: Carne(), pan: Pan()))
+        print(hamburguesas)
     }
 }
 

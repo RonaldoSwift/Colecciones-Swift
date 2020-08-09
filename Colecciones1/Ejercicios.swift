3b4c1074714e6d3cb1a5e6414f5700760e7a5c5b
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-08-09 13:26:43 -0500

    3 Ejercicios sobre composición

---
+++ Colecciones1/Ejercicios.swift
@@ -0,0 +1,46 @@
+//
+//  Ejercicios.swift
+//  Colecciones1
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 8/9/20.
+//  Copyright © 2020 Ronaldo Andre Vargas Huaman. All rights reserved.
+//
+
+import Foundation
+
+/* 1
+ Crear un archivo en swift llamado Hamburguesa. Dentro del archivo crear las siguiente estructuras:
+ - Hamburguesa
+ - Lechuga
+ - Carne
+ - Tomate
+ - Papa
+ 
+ La Hamburguesa esta compuesta por 1 lechuga, 1 carne, 1 tomate y 1 papa.
+ Aplicar la relacion de clases "Composición".
+ Finalmente crear un arreglo de Hamburguesas
+ */
+
+
+/* 2
+ Crear un archivo en swift llamado Carro. Dentro del archivo crear las siguientes estructuras:
+ - Carro
+ - Rueda
+ - Asiento
+ - Puerta
+
+ El carro esta compuesto por 4 ruedas, 5 asientos, y 4 puertas.
+ Aplicar composición.
+ Finalmente crear un solo objeto de carro.
+ */
+
+/* 3
+ Crear un archivo en swift llamada Restaurante. Dentro del archivo crear las siguientes estructuras:
+ - Restaurante
+ - Cliente
+ - Empleado
+ 
+ El restaurante esta compuesta por 1 arreglo de Cliente y 1 arreglo de Empleado.
+ Aplicar composición
+ Finalmente crear un resturante.
+ */
